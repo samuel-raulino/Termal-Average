3acfea86a9f4580fc56cc2de23c15b2524304f12
samuel-raulino <113915653+samuel-raulino@users.noreply.github.com>
2024-08-02 11:43:06 -0300

    Create atv1.py

---
+++ atv1.py
@@ -0,0 +1,148 @@
+
+import random,os
+
+def create_doubleList(a,b):
+    matriz = {}
+    for x in range(a):
+        matriz[x] = []
+        for xx in range(b):
+            matriz[x].append(xx)
+    return matriz
+
+def create_list(a):
+    vetor = []
+    for x in range(a):
+        vetor.append(x)
+    return vetor
+
+horas = 24 
+dia = 8
+mes = 12
+
+mediadia = create_doubleList(dia,horas)
+
+mediasmes = create_doubleList(mes,dia)
+mediames = create_list(mes)
+maior = 0
+menor = 0
+datad = 0
+datam = 0
+enter = ""
+os.system("cls")
+print("Bem vindo ao Thermal Average!\n\n")
+print("O Thermal Average é um programa que registra as temperaturas diárias de um pais ao longo dos meses.\n")
+print("Aqui te mostraremos: média mensal de 8 dias dos meses, o dia e mês mais quente/frio do ano e a média da temperatura. ")
+print("\n\nAperte enter para começar ")
+enter = input()
+os.system("cls")
+    
+for i in range(dia):
+    mediames[i] = 0
+
+for i in range(mes):
+    for ii in range(dia):
+        mediasmes[i][ii] = 0
+      
+    
+print("Temperatura de todos os meses:\n")
+print("(Aperte enter para continuar) ")
+pais = input("Qual o pais que você deseja escolher?")
+enter = input()
+os.system("cls")
+def mediaanual(mininverno,maxinverno,minverao,maxverao,inverno,primavera,outono,verao):
+    for i in range(dia):
+        for ii in range(horas):
+            if m+1 in inverno:
+                mediadia[i][ii] = random.randint(mininverno,maxinverno)
+            elif m+1 in outono:
+                mediadia[i][ii] = random.randint(minverao-10,maxverao-10)
+            elif m+1 in primavera:
+                mediadia[i][ii] = random.randint(mininverno+5,maxinverno+5)
+            elif m+1 in verao:
+                mediadia[i][ii] = random.randint(minverao,maxverao)
+for m in range(mes):
+    if pais == "EUA":
+        mediaanual(0,20,20,40,[12,1,2],[3,4,5],[9,10,11],[6,7,8])
+    
+    #todas as medias de todos os meses
+    print(" Mês ",m+1,":\n")
+    for i in range(dia):
+        for ii in range(horas):
+            mediasmes[m][i]+= mediadia[i][ii]
+        mediasmes[m][i]/= horas 
+        print(" Dia ",i+1,": ",int(mediasmes[m][i]),"°C \n")
+      
+print(" (Aperte enter para continuar) ")
+enter = input()
+os.system("cls")
+print("\n")
+
+print("(Aperte enter para continuar) ")
+enter = input()
+os.system("cls")
+print(" Temperatura média de todos os meses: ")
+
+    #separação
+for i in range(mes):
+    mediames[i] = 0
+    for ii in range(dia):
+        mediames[i] += mediasmes[i][ii]
+        
+    mediames[i]/=dia 
+    print("\n Mês: ",i+1," \n Média de temperatura: ",int(mediames[i]),"°C")
+    print("\n (Aperte enter para continuar) ")
+    enter = input()
+    os.system("cls")
+    
+    #separação 2
+print("\n Data do dia mais quente do ano: ")
+for i in range(mes):
+    for ii in range(7):
+        if maior < mediasmes[i][ii]:
+            maior = mediasmes[i][ii]
+            datad = ii+1
+            datam = i+1
+          
+
+print("Dia: ",datad," Mes:",datam,"\n Média de temperatura no dia: ",int(maior),"°C")
+enter = input()
+os.system("cls")
+#menor temperatura do ano
+print("\n Data do dia mais frio do ano: ")
+menor = maior
+for i in range(mes):
+    for ii in range(7):
+        if menor > mediasmes[i][ii]:
+            menor = mediasmes[i][ii]
+            datad = ii+1
+            datam = i+1
+
+print("Dia: ",datad," Mes: ",datam,"\n Média de temperatura no dia: ",int(menor),"°C")
+enter = input()
+
+os.system("cls")
+maior = 0
+
+for i in range(mes):
+    if maior < mediames[i]:
+        maior = mediames[i]
+        datam = i+1
+print(" Mês mais quente do ano: ",datam)
+print( "\n Média de temperatura no mês: ",int(maior),"°C\n")
+enter = input()
+os.system("cls")
+
+
+menor = maior
+for i in range(mes):
+    if menor > mediames[i]:
+        menor = mediames[i]
+        datam = i+1
+print("\n Mês mais frio do ano: ",datam)
+print("\n Média de temperatura no mês: ",int(menor),"°C\n")
+
+enter = input()
+os.system("cls")
+print("Fim do programa!")
+enter = input()
+os.system("cls")
